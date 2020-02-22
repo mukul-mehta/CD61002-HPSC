54e720e86d45f54e8e3ee7e33cf84f3d743e6147
Soumick-Pyne <42906929+Soumick-Pyne@users.noreply.github.com>
2020-02-22 17:00:25 +0530

    Add files via upload

--- jacobi.cpp
+++ jacobi.cpp
@@ -12,34 +12,68 @@ int main()
 	{
 		int i, j;
 		double l = 0;
-		const int n = 3;
-		double a[n][n], b[n][1], x[n][1], T[n][1], e, k;
-		cout << "[a].[x]=[b]" << endl;
-		cout << "Enter Matrix a:" << endl;
-		for (i = 0; i < n; i++)
-			for (j = 0; j < n; j++)
-			{
-				cout << "a[" << i << "," << j << "] = ";
-				cin >> a[i][j];
-			}
-		cout << "Enter Matrix b:" << endl;
-		for (i = 0; i < n; i++)
+		const int n = 6;
+		double a[n][n], b[n][1], x[n][1], T[n][1], e, k, I[n][4], IOFF[1][4];
+		int tempe;
+
+		for (int i = 0; i < n; i++)
 		{
-			cout << "b[" << i << "][0] = ";
+			cout << "Enter b [" << i << "]";
 			cin >> b[i][0];
 		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				cout << "DIAG [" << i << "][" << j << " ";
+				cin >> I[i][j];
+			}
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			cout << "Enter IOFF [" << i << "]";
+			cin >> IOFF[0][i];
+		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				a[i][j] = 0;
+			}
+		}
+
 		cout << "Enter the Accuracy = ";
 		cin >> e;
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{	
+				tempe = IOFF[0][j];
+				if (I[i][j] == 1000)
+					continue;
+				a[i][i+tempe] = I[i][j];
+			}
+		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				cout << "a[" << i << "][" << j << "]" << "=" << a[i][j] << "  ";
+			}
+			cout << "\n";
+		}
+
 		for (i = 0; i < n; i++)
 			T[i][0] = 0;
 		l = 0;
 		while (1)
-		{
+		{	
+			l = 0;
 			cout << "\nIteration no: " << l << "\n";
 			for (i = 0; i < n; i++)
 			{
 				x[i][0] = (1 / a[i][i])*(b[i][0]);
-				//cout << x[i][0] << " ";
 				for (j = 0; j < n; j++)
 				{
 					if (j != i)
@@ -48,16 +82,13 @@ int main()
 				}
 				//cout << "\n";
 			}
-			k = 0;
 			for (i = 0; i < n; i++)
 			{
-				k += pow(fabs(x[i][0] - T_dash[i][0]), 2);
+				if (fabs(x[i][0] - T[i][0]) < e);
+				l++;
 			}
-			if (k <= e)
-			{
+			if (l >= n)
 				break;
-			}
-			l++;
 			for (i = 0; i < n; i++)
 				T[i][0] = x[i][0];
 
@@ -67,26 +98,6 @@ int main()
 		for (i = 0; i < n; i++)
 			cout << "x" << i + 1 << "=" << x[i][0] << endl;
 
-		//trying spectral radius calculation
-		double p_G = 0, sum1 = 0, sum2 = 0, base = 0, expo = 0;
-		for (i = 0; i < n; i++)
-		{
-			sum1 += (x[i][0] - T[i][0])*(x[i][0] - T[i][0]);
-		}
-		cout << sum1 << "\n";
-		for (i = 0; i < n; i++)
-		{
-			sum2 += x[i][0] * x[i][0];
-		}
-		cout << sum2 << "\n";
-		base = sum1 / sum2;
-		cout << " base = " << base;
-		expo = 1 / l;
-		cout << "expo = " << expo;
-		p_G = pow(base, expo);
-		cout << "\nspectral radius of G matrix = " << p_G;
-		cout << "\ncontinue (1 or 0)?";
-		cin >> ans;
 	};
 	_getch();
 	return 0;
